b8ea417237d368ceb13bb7c4dcef037c7cb49363
Willian Dantas <williansilva1991@gmail.com>
2021-03-31 16:09:35 -0300

    05. Herança e o Liskov Substitutive Principle

---
+++ SolidLiskov/ContaComum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidLiskov
+{
+    public class ContaComum
+    {
+        public double Saldo { get; protected set; }
+
+      
+ 
+
+        public virtual void Deposita(double valor)
+        {
+            this.Saldo += valor;
+        }
+
+        public void Saca(double valor)
+        {
+            if(valor <= this.Saldo)
+            {
+                this.Saldo -= valor;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public virtual void SomaInvestimento()
+        {
+            if(this.Saldo > 0)
+            {
+                this.Saldo += this.Saldo * 0.01;
+            }
+            
+        }
+
+    }
+}
---
+++ SolidLiskov/ContaEstudante.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidLiskov
+{
+    public class ContaEstudante : ContaComum
+    {
+        public int Milhas { get; private set; }
+
+        public override void Deposita(double valor)
+        {
+            base.Deposita(valor);
+            this.Milhas += (int)valor;
+        }
+
+        
+    }
+}
